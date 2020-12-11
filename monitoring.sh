77b1146e29140a1891fd6588a78d92705bc7109c
kodeeshm <38654198+kodeeshm@users.noreply.github.com>
2020-12-11 14:47:14 +0530

    Adding the Monitoring shell script

---
+++ monitoring.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+DATE=$(date +'%Y-%m-%d')
+LOGNAME=$(date +"%Y-%m-%d-%T" | sed -e 's/:/'_'/g')
+
+# Write a log file with details
+DEVICENAME=`hostname`
+TS=$(echo $DATE_$LOGNAME)
+FILE=`echo $DEVICENAME-$TS.log`
+
+echo -e "***** HOST INFORMATION *****" >> $FILE
+hostnamectl >> $FILE
+echo "" >> $FILE
+echo "-------------------------------" >> $FILE
+# -Dropped packets details
+echo -e "***** Dropped Packets *****" >> $FILE
+netstat -s |grep -i dropped >> $FILE
+echo "" >> $FILE
+echo "-------------------------------" >> $FILE
+# -tx details:
+echo -e "***** tx_packets and rx_packets *****" >> $FILE
+ip -s link >> $FILE
+echo "" >> $FILE
+echo "-------------------------------" >> $FILE
+# -System uptime:
+echo -e "***** SYSTEM UPTIME *****" >> $FILE
+uptime >> $FILE
+echo "" >> $FILE
+echo "-------------------------------" >> $FILE
+echo -e "Done..." >> $FILE

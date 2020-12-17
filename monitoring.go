165e106d85c9c9b5f2270080aef5bf0b2a0cb198
kodeeshm <38654198+kodeeshm@users.noreply.github.com>
2020-12-17 22:41:04 +0530

    Update monitoring.go

--- monitoring.go
+++ monitoring.go
@@ -3,42 +3,80 @@ package main
 import (
     "fmt"
     "os/exec"
+    "bufio"
+    "os"
+    "time"
 )
 
-func main() {
-    //Hostname Information
-	out, err := exec.Command("hostnamectl").Output()
+func check_err(err error) {
     if err != nil {
         fmt.Printf("%s", err)
+        // panic(err)
     }
-    fmt.Println("Host information collected...")
+}
+
+func main() {
+
+    // frame the output filename
+    out, err := exec.Command("hostname").Output()
+    check_err(err)
+
+    currentTime := time.Now()
+    current_date := currentTime.Format("01_02_2006")
+    epoch_secs := currentTime.Unix()
+
+    output_filename := out + "_" + current_date + "_" + epoach_secs + ".log"
+
+    // create output log file if it's not exist
+    file, err := os.OpenFile(output_filename, os.O_WRONLY|os.O_CREATE, 0666)
+    if err != nil {
+        fmt.Println("File does not exists or cannot be created")
+        os.Exit(1)
+    }
+    defer file.Close()
+
+    w := bufio.NewWriter(file)
+
+    //Hostname Information
+    out, err = exec.Command("hostnamectl").Output()
+    check_err(err)
+
+    fmt.Fprintf(w, "%s\n", "Host information collected...")
+    w.Flush()
     output := string(out[:])
     fmt.Println(output)
-	
-	//Dropped packet details
-	out, err = exec.Command("netstat", "-s", "|grep", "-i", "dropped").Output()
-    if err != nil {
-        fmt.Printf("%s", err)
-    }
-    fmt.Println("Dropped packets details...")
+    fmt.Fprintf(w, "%s\n", output)
+    w.Flush()
+
+    //Dropped packet details
+    cmd := "netstat -s | grep -i dropped"
+    out, err = exec.Command("bash","-c",cmd).Output()
+    check_err(err)
+
+    fmt.Fprintf(w, "%s\n", "Dropped packets details...")
     output = string(out[:])
     fmt.Println(output)
-	
-	//Tx and Rx packets details
-	out, err = exec.Command("ip", "-s", "link").Output()
-    if err != nil {
-        fmt.Printf("%s", err)
-    }
-    fmt.Println("Tx and Rx packets details...")
+    fmt.Fprintf(w, "%s\n", output)
+    w.Flush()
+
+    //Tx and Rx packets details
+    out, err = exec.Command("ip", "-s", "link").Output()
+    check_err(err)
+
+    fmt.Fprintf(w, "%s\n", "Tx and Rx packets details...")
     output = string(out[:])
     fmt.Println(output)
-	
-	//System Uptime information
-	out, err = exec.Command("uptime").Output()
-    if err != nil {
-        fmt.Printf("%s", err)
-    }
-    fmt.Println("Uptime Information...")
+    fmt.Fprintf(w, "%s\n", output)
+    w.Flush()
+
+    //System Uptime information
+    out, err = exec.Command("uptime").Output()
+    check_err(err)
+
+    fmt.Fprintf(w, "%s\n", "Uptime Information...")
     output = string(out[:])
     fmt.Println(output)
+    fmt.Fprintf(w, "%s\n", output)
+    w.Flush()
 }
+
